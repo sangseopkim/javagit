1018c4d877cf3ed2f9e25dc831a5a34bf76e5016
sangseopkim <cy****3@gmail.com>
2020-11-08 18:55:34 +0900

    temp4-1

--- src/javagit/Test1.java
+++ src/javagit/Test1.java
@@ -4,5 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test1234");
+		// 배고프다.. 밥먹고 해야지..
+		System.out.println("0000");
 	}
 }
